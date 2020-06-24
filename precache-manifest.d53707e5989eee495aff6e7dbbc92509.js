9bb2a21171f4fe8731698a8cfa7c3810a8d48621
forceExists <nsuyash11@gmail.com>
2020-06-25 00:34:25 +0530

    Updates

---
+++ precache-manifest.d53707e5989eee495aff6e7dbbc92509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3769cac27ec2c38c12366a24bc29f4a",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e2fdcee4c940e43734d4",
+    "url": "/todolist/static/css/main.8121cfc9.chunk.css"
+  },
+  {
+    "revision": "21eadae433675f856bf1",
+    "url": "/todolist/static/js/2.7b0e63ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist/static/js/2.7b0e63ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2fdcee4c940e43734d4",
+    "url": "/todolist/static/js/main.c1322db5.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
